aa10c86ae739d23a254c91923822ad5b4187e5a3
Thiago <tmelo.mat@gmail.com>
2018-06-01 16:55:54 -0300

    Add files via upload

--- delimctr.sh
+++ delimctr.sh
@@ -29,56 +29,55 @@
 
 clear
 input_file=$1
-echo -e "Checking delimiters on file \033[1;34m${input_file}"
 
-o_brace=$(grep  -o "{" ${input_file} | wc -l)
-c_brace=$(grep  -o "}" ${input_file} | wc -l)
+printf "Checking delimiters on file: \\033[1;34m%s\\n" "${input_file}"
+
+o_brace=$(grep  -o '{' "${input_file}" | wc -l)
+c_brace=$(grep  -o '}' "${input_file}" | wc -l)
 gap_brace=$(( o_brace - c_brace ))
 
-o_bracket=$(grep  -o "\[" ${input_file} | wc -l)
-c_bracket=$(grep  -o "\]" ${input_file} | wc -l)
+o_bracket=$(grep  -o '\[' "${input_file}" | wc -l)
+c_bracket=$(grep  -o '\]' "${input_file}" | wc -l)
 gap_bracket=$(( o_bracket - c_bracket ))
 
-o_paren=$(grep  -o "(" ${input_file} | wc -l)
-c_paren=$(grep  -o ")" ${input_file} | wc -l)
+o_paren=$(grep  -o '(' "${input_file}" | wc -l)
+c_paren=$(grep  -o ')' "${input_file}" | wc -l)
 gap_paren=$(( o_paren - c_paren ))
 
 printf '\033[1;37m'""
-printf             "\n  opening parentheses (  ${o_paren}"
-printf             "\nclosening parentheses )  ${c_paren}"
+printf             "\\n  opening parentheses (  %d" "${o_paren}"
+printf             "\\nclosening parentheses )  %d" "${c_paren}"
 [ $gap_paren != 0 ] && 
-printf '\033[1;31m'"\n     missing or extra    ${gap_paren#-}"
+printf '\033[1;31m'"\\n     missing or extra    %d" "${gap_paren#-}"
 
 xymatrix=0
-[ $gap_paren != 0 ] && xymatrix=$(grep -c -o "xymatrix" ${input_file} )
-if [ $xymatrix != 0 ]; then
-    l_hook=$(grep -o "\^{(}" ${input_file} | wc -l)
-    r_hook=$(grep -o "\^{)}" ${input_file} | wc -l)
-    l_hook_lineno=$(grep -n "\^{(}" ${input_file} | grep -Eo '^[^:]+')
-    r_hook_lineno=$(grep -n "\^{)}" ${input_file} | grep -Eo '^[^:]+')
-    [ $r_hook != 0 ] || [ $l_hook != 0 ] && printf "\n\n** xymatrix inclusion arrows found; it uses unpaired ( )\n** see lines: "
+[ $gap_paren != 0 ] && xymatrix=$(grep -c -o 'xymatrix' "${input_file}" )
+if [ "$xymatrix" != 0 ]; then
+    l_hook=$(grep -o '\^{(}' "${input_file}" | wc -l)
+    r_hook=$(grep -o '\^{)}' "${input_file}" | wc -l)
+    l_hook_lineno=$(grep -n '\^{(}' "${input_file}" | grep -Eo '^[^:]+')
+    r_hook_lineno=$(grep -n '\^{)}' "${input_file}" | grep -Eo '^[^:]+')
+    [ "$r_hook" != 0 ] || [ "$l_hook" != 0 ] && printf "\\n\\n** xymatrix inclusion arrows found; it uses unpaired ( )\\n** see lines: "
     echo -n $l_hook_lineno $r_hook_lineno
 fi
 
 item=0
-[ $gap_paren != 0 ] && item=$(grep -c -E '\s*\[\s*\w+\)\s*\]' ${input_file})
-#item=$(tr -d " \t" < ${input_file} | grep -c '\[[0-9A-Za-z])\]'  )
-if [ $item != 0 ]; then
-    item_lineno=$(grep  -n -E '\s*\[\s*\w+\)\s*\]' ${input_file} | grep -Eo '^[^:]+' )
-    #$(tr -d " \t" < ${input_file} | grep -n '\[[0-9A-Za-z])\]' | grep -Eo '^[^:]+')
-    printf "\n\n** labeled items found; it could use unpaired ( )\n** see lines: "
+[ "$gap_paren" != 0 ] && item=$(grep -c -E '\s*\[\s*\w+\)\s*\]' "${input_file}")
+if [ "$item" != 0 ]; then
+    item_lineno=$(grep -n -E '\s*\[\s*\w+\)\s*\]' "$input_file" | grep -Eo '^[^:]+')
+    printf "\\n\\n** labeled items found; it could use unpaired ( )\\n** see lines: "
     echo -n $item_lineno
 fi
 
-printf '\033[1;37m'"\n"
-printf             "\n  opening brackets [  ${o_bracket}"
-printf             "\nclosening brackets ]  ${c_bracket}"
+printf '\033[1;37m'"\\n"
+printf             "\\n  opening brackets [  %d" "${o_bracket}"
+printf             "\\nclosening brackets ]  %d" "${c_bracket}"
 [ $gap_bracket != 0 ] && 
-printf '\033[1;31m'"\n missing or extra    ${gap_bracket#-}"
+printf '\033[1;31m'"\\n  missing or extra    %d" "${gap_bracket#-}"
 
-printf '\033[1;37m'"\n"
-printf             "\n  opening braces { ${o_brace}"
-printf             "\nclosening braces } ${c_brace}"
+printf '\033[1;37m'"\\n"
+printf             "\\n  opening braces { %d" "${o_brace}"
+printf             "\\nclosening braces } %d" "${c_brace}"
 [ $gap_brace != 0 ] && 
-printf '\033[1;31m'"\nmissing or extra   ${gap_brace#-}"
-printf '\033[0;37m'"\n\n"
+printf '\033[1;31m'"\\nmissing or extra   %d" "${gap_brace#-}"
+printf '\033[0;37m'"\\n\\n"

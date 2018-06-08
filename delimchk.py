000e47ac760d6426e0b939b13a8c191258335dbf
Thiago <tmelo.mat@gmail.com>
2018-06-07 22:07:06 -0300

    Add files via upload

--- delimchk.py
+++ delimchk.py
@@ -1,3 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+# Copyright (c) 2018, Thiago de Melo. All rights reserved.
+# Thanks to people from 
+# https://stackoverflow.com/a/29992065/1778537
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+# Redistributions of source code must retain the above copyright notice, 
+# this list of conditions and the following disclaimer.
+#
+# Redistributions in binary form must reproduce the above copyright notice, 
+# this list of conditions and the following disclaimer in the documentation 
+# and/or other materials provided with the distribution.
+# Neither the name of the {organization} nor the names of its contributors may 
+# be used to endorse or promote products derived from this software without 
+# specific prior written permission.
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+# GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) 
+# HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+# LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY 
+# OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+"""
+
 def is_matched(expression):
     """
     Finds out how balanced an expression is.
@@ -23,7 +53,8 @@ def is_matched(expression):
 
 def find_parens(s):
     ''' 
-    https://stackoverflow.com/a/29992065/1778537
+    
+    ?problem if input_file has a single line?
     '''
     
     toret = {}
@@ -43,10 +74,19 @@ def find_parens(s):
     if len(pstack) > 0:
         tmp = 0
         for i in pstack:
-            print s[tmp:i+1],"<--- no matching\n"
-            
+            j = i+1
+            while s[j] != "\n":
+                j -= 1
+            print s[j+1:i+1]
+            #print u"\u25B2".encode("utf-8")u"  \u2190".encode("utf-8")+
+            arrow = ' '*(i-j-1)+u"\u25B2".encode("utf-8")+'  *** no matching' # U+21Bx U+219x
+            print arrow
             tmp = i+1
-        print s[tmp:]
+            j = tmp
+            while s[j] != "\n":
+                j += 1
+            print s[tmp:j]
+            print 
         
         #raise IndexError("No matching opening parens at: " + str(pstack.pop()))
 
@@ -57,11 +97,22 @@ if __name__ == '__main__':
     #import doctest
     #doctest.testmod()
     
+    delimchk="delimchk"
+    author="Thiago de Melo"
+    github="https://github.com/tmelorc/delimctr"
+    text1 = "This is {delimchk}. Created by {author}.".format(author=author, delimchk=delimchk.upper())
+    text2 = "Visit {github} for updates.".format(github=github)
+    dashed_line = "-"*len(text2)
+    print '%s\n%s\n%s\n' % (text1,text2,dashed_line)
+    
     input_file = sys.argv[1]
+    #input_file = '/home/thiago/fastex-temp.tex'
     
     with open(input_file, 'r') as myfile:
         data = myfile.read()
     
-    print "Matching delimiters? ", is_matched(data), "\n"
+    match = is_matched(data)
+    print "Matching delimiters? %s\n" % str(match).upper()
+    
     #print "There is/are %d non matching", len(pstack)
-    find_parens(data)
+    if not match: find_parens(data)
